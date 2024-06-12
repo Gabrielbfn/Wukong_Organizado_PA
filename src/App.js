ecec35b0a336bc4e8923494cf4a74751a9a188bd
MARIA EDUARDA FERNANDES LIMA <maria.eflima6@senacsp.edu.br>
2024-06-12 18:02:06 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -13,10 +13,13 @@ function Home() {
 
         <div className='botao-home'>
 
-        <button className='jogar-buttom' type="jogar"> 
-          <img className='jogar-bottao' src="./" />
+        <button className='jogar-butom'>
+        <img  class="imagem_botao_jogar" src='https://cdn.discordapp.com/attachments/1240374436071473224/1250554098671943764/jogar.png?ex=666b5cc3&is=666a0b43&hm=11b9faf2682fae72c8b532304652710869ee76f7ab45ac0d934e1b669996b551&' />
         </button>
-{/* 
+          {/* <button className='jogar-butom'>
+            <img src='..src/assets/images/1713991542711.webp' />
+          </button>
+
         <button type="novo-jogo"> 
           <img src="/src/assets/imagens/1713991542711.png" />
         </button>
